bc88b7f1168f1a76859327afa81bd499f1a7f906
jj <yang@yang.com>
2017-02-27 10:41:06 +0800

    initial project

---
+++ src/main/java/com/peraglobal/dataprocess/DataProcessApplication.java
@@ -0,0 +1,12 @@
+package com.peraglobal.dataprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessApplication.class, args);
+	}
+}
