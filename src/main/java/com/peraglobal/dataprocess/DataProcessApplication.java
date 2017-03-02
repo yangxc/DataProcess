861d6da2d235749cbaaae32e000f639c1af1bfc7
yongqian.liu <yongqian.liu@peraglobal.com>
2017-03-02 11:16:56 +0800

    增加：数据转换服务依赖 jar

--- src/main/java/com/peraglobal/dataprocess/DataProcessApplication.java
+++ src/main/java/com/peraglobal/dataprocess/DataProcessApplication.java
@@ -2,11 +2,43 @@ package com.peraglobal.dataprocess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+import com.peraglobal.common.CurrentApplicationContext;
+
+/**
+ * <code>DataProcessApplication.java</code>
+ * <p>
+ * 功能:启动类
+ * <p>
+ * Copyright 安世亚太 2017 All right reserved.
+ * @author yongqian.liu
+ * @version 1.0 2017-2-28 </br>
+ * 			最后修改人 无
+ */
 @SpringBootApplication
 public class DataProcessApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataProcessApplication.class, args);
+		// 设置启动类
+		ApplicationContext applicationContext = SpringApplication.run(DataProcessApplication.class, args);
+		// 设置 spring 上下文到自定义类中
+		CurrentApplicationContext.setApplicationContext(applicationContext);
 	}
+	
+	/**
+	 * 设置 RestTemplate 功能，在项目中调用 RESTFul API
+	 * 
+	 * @param builder
+	 * @return
+	 */
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 }
+
